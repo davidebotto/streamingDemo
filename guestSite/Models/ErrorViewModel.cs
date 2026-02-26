1d588112c3c2028b0a75aa63d5793c7f7fe7f387
Davide Botto <davide.botto@infosys.it>
2026-02-26 18:09:36 +0100

    Testing custom element

--- guestSite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace guestSite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
