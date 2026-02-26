e4cf48b3c389c95fa4b596563ee0a1c6b308e74e
Davide Botto <davide.botto@infosys.it>
2026-02-26 17:53:55 +0100

    FE project .NET9

--- streamingDemo_FE/Program.cs
+++ streamingDemo_FE/Program.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using streamingDemo_FE;
-using streamingDemo_FE.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.RootComponents.RegisterCustomElement<ChatStream>("chat-stream");
 
 await builder.Build().RunAsync();
